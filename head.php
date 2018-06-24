ecc4c622f039188b735d33cd823bdbf509262e26
Toma Loredana <tomaloredana@gmail.com>
2018-06-24 16:24:49 +0300

    about page

--- head.php
+++ head.php
@@ -1,6 +1,23 @@
+
+<html class="no-js" lang="">
+
 <head>
     <title>Alecto</title>
-    <meta charset="UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  
+  
   <link href="css/style.css" rel="stylesheet">
   <link rel="stylesheet" href="css/bootstrap.min.css"> 
   <link rel="stylesheet"href="css/style.css"> 
